3a0513deb48164233f643c1a092f47555e0ada5a
vmkvsite <vikronja@vsite.hr>
2025-05-27 18:01:04 +0200

    gotova vjezba

--- main.cpp
+++ main.cpp
@@ -1,42 +1,107 @@
 #include "main.h"
 #include "rc.h"
 
+int board_width = 8;
+int board_height = 8;
+COLORREF color1 = RGB(255, 255, 0); 
+COLORREF color2 = RGB(255, 255, 255);
+
 int size_dialog::idd() const {
-	return IDD_SIZE; 
+	return IDD_SIZE;
 }
 
-bool size_dialog::on_init_dialog(){
+bool size_dialog::on_init_dialog() {
+	set_int(IDC_EDIT1, board_width);
+	set_int(IDC_EDIT2, board_height);
 	return true;
 }
 
-bool size_dialog::on_ok(){
-	return true;
+bool size_dialog::on_ok() {
+	try {
+		int new_width = get_int(IDC_EDIT1);
+		int new_height = get_int(IDC_EDIT2);
+
+		if (new_width > 0 && new_height > 0 && new_width <= 20 && new_height <= 20) {
+			board_width = new_width;
+			board_height = new_height;
+			return true;
+		}
+	}
+	catch (...) {
+	}
+	return false;
 }
 
+void main_window::on_paint(HDC hdc) {
+	RECT client_rect;
+	GetClientRect(*this, &client_rect);
 
-void main_window::on_paint(HDC hdc){
-}
+	int cell_width = (client_rect.right - client_rect.left) / board_width;
+	int cell_height = (client_rect.bottom - client_rect.top) / board_height;
 
-void main_window::on_command(int id){
-	switch(id){
-		case ID_SIZE:
-			break;
-		case ID_COLOR:
-			break;
-		case ID_EXIT: 
-			DestroyWindow(*this); 
-			break;
+	for (int row = 0; row < board_height; row++) {
+		for (int col = 0; col < board_width; col++) {
+
+			COLORREF current_color = ((row + col) % 2 == 0) ? color1 : color2;
+
+			HBRUSH brush = CreateSolidBrush(current_color);
+			HBRUSH old_brush = (HBRUSH)SelectObject(hdc, brush);
+
+			RECT cell_rect;
+			cell_rect.left = col * cell_width;
+			cell_rect.top = row * cell_height;
+			cell_rect.right = (col + 1) * cell_width;
+			cell_rect.bottom = (row + 1) * cell_height;
+
+			FillRect(hdc, &cell_rect, brush);
+
+			SelectObject(hdc, old_brush);
+			DeleteObject(brush);
+		}
 	}
 }
 
-void main_window::on_destroy(){
+void main_window::on_command(int id) {
+	switch (id) {
+	case ID_SIZE:
+	{
+		size_dialog dlg;
+		if (dlg.do_modal(GetModuleHandle(NULL), *this) == IDOK) {
+			InvalidateRect(*this, NULL, TRUE);
+		}
+	}
+	break;
+	case ID_COLOR:
+	{
+		CHOOSECOLOR cc = {};
+		static COLORREF custom_colors[16] = {};
+
+		cc.lStructSize = sizeof(cc);
+		cc.hwndOwner = *this;
+		cc.rgbResult = color1;
+		cc.lpCustColors = custom_colors;
+		cc.Flags = CC_FULLOPEN | CC_RGBINIT;
+
+		if (ChooseColor(&cc)) {
+			color1 = cc.rgbResult;
+			InvalidateRect(*this, NULL, TRUE);
+		}
+	}
+	break;
+	case ID_EXIT:
+		DestroyWindow(*this);
+		break;
+	}
+}
+
+void main_window::on_destroy() {
 	::PostQuitMessage(0);
 }
 
 int WINAPI WinMain(HINSTANCE hi, HINSTANCE, LPSTR, int)
 {
 	vsite::nwp::application app;
-	main_window wnd;	
+	main_window wnd;
 	wnd.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, _T("NWP"), (UINT_PTR)LoadMenu(hi, MAKEINTRESOURCE(IDM_MAIN)));
 	return app.run();
 }
